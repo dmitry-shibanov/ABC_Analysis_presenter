35a4fcb7966158571452189b6be3183e24684a13
Dima Shibanov <shibanov-1997@inbox.ru>
2020-06-04 14:40:28 +0300

    intermidiate changes

--- ABC_Analysis_presenter/forms/ABCResultForm.cs
+++ ABC_Analysis_presenter/forms/ABCResultForm.cs
@@ -8,15 +8,110 @@ namespace ABC_Analysis_presenter
     public partial class ABCResultForm : Form
     {
         private MainWindow mainForm;
+        DataGridView[] tables;
+
         public ABCResultForm(MainWindow mainForm)
         {
             InitializeComponent();
+            tables = new DataGridView[] { axGrid, bxGrid, cxGrid, ayGrid, byGrid, cyGrid, azGrid, bzGrid, czGrid };
             this.mainForm = mainForm;
             List<ServiceModel> list = getListFromGrid();
             if (list.Count > 1)
             {
                 list = calcABC(list);
                 setGroupsOnForm(list);
+            }
+        }
+
+        public void Export_Data_To_Word(DataGridView DGV, Word._Document oDoc, int index)
+        {
+            if (DGV.Rows.Count != 0)
+            {
+                int RowCount = DGV.Rows.Count;
+                int ColumnCount = DGV.Columns.Count;
+                Object[,] DataArray = new object[RowCount + 1, ColumnCount + 1];
+
+                //add rows
+                int r = 0;
+                for (int c = 0; c <= ColumnCount - 1; c++)
+                {
+                    for (r = 0; r <= RowCount - 1; r++)
+                    {
+                        DataArray[r, c] = DGV.Rows[r].Cells[c].Value;
+                    } //end row loop
+                } //end column loop
+
+                //Word.Document oDoc = new Word.Document();
+                //oDoc.Application.Visible = true;
+
+                //page orintation
+                oDoc.PageSetup.Orientation = Word.WdOrientation.wdOrientLandscape;
+
+
+                dynamic oRange = oDoc.Content.Application.Selection.Range;
+                string oTemp = "";
+                for (r = 0; r <= RowCount - 1; r++)
+                {
+                    for (int c = 0; c <= ColumnCount - 1; c++)
+                    {
+                        oTemp = oTemp + DataArray[r, c] + "\t";
+
+                    }
+                }
+
+                //table format
+                oRange.Text = oTemp;
+
+                object Separator = Word.WdTableFieldSeparator.wdSeparateByTabs;
+                object ApplyBorders = true;
+                object AutoFit = true;
+                object AutoFitBehavior = Word.WdAutoFitBehavior.wdAutoFitContent;
+
+                oRange.ConvertToTable(ref Separator, ref RowCount, ref ColumnCount,
+                                      Type.Missing, Type.Missing, ref ApplyBorders,
+                                      Type.Missing, Type.Missing, Type.Missing,
+                                      Type.Missing, Type.Missing, Type.Missing,
+                                      Type.Missing, ref AutoFit, ref AutoFitBehavior, Type.Missing);
+
+                oRange.Select();
+
+                oDoc.Application.Selection.Tables[1].Select();
+                oDoc.Application.Selection.Tables[1].Rows.AllowBreakAcrossPages = 0;
+                oDoc.Application.Selection.Tables[1].Rows.Alignment = 0;
+                oDoc.Application.Selection.Tables[1].Rows[1].Select();
+                oDoc.Application.Selection.InsertRowsAbove(1);
+                oDoc.Application.Selection.Tables[1].Rows[1].Select();
+
+                //header row style
+                oDoc.Application.Selection.Tables[1].Rows[1].Range.Bold = 1;
+                oDoc.Application.Selection.Tables[1].Rows[1].Range.Font.Name = "Tahoma";
+                oDoc.Application.Selection.Tables[1].Rows[1].Range.Font.Size = 14;
+
+                //add header row manually
+                for (int c = 0; c <= ColumnCount - 1; c++)
+                {
+                    oDoc.Application.Selection.Tables[1].Cell(1, c + 1).Range.Text = DGV.Columns[c].HeaderText;
+                }
+
+                //table style 
+                //oDoc.Application.Selection.Tables[1].set_Style("Grid Table 4 - Accent 5");
+                oDoc.Application.Selection.Tables[1].Rows[1].Select();
+                oDoc.Application.Selection.Cells.VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+                //header text
+                foreach (Word.Section section in oDoc.Application.ActiveDocument.Sections)
+                {
+                    Word.Range headerRange = section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                    headerRange.Fields.Add(headerRange, Word.WdFieldType.wdFieldPage);
+                    headerRange.Text = "your header text";
+                    headerRange.Font.Size = 16;
+                    headerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                }
+
+                //save the file
+                //oDoc.SaveAs2(filename);
+
+                //NASSIM LOUCHANI
             }
         }
 
@@ -31,10 +126,19 @@ namespace ABC_Analysis_presenter
             oWord.Visible = true;
             oDoc = oWord.Documents.Add(ref oMissing, ref oMissing,
             ref oMissing, ref oMissing);
-
+            int index = 0;
+            foreach(DataGridView item in tables)
+            {
+                if(item.Rows.Count != 0)
+                {
+                    index++;
+                }
+                Export_Data_To_Word(item, oDoc, index);
+            }
+            /*
             Word.Paragraph oPara1;
             oPara1 = oDoc.Content.Paragraphs.Add(ref oMissing);
-            oPara1.Range.Text = "Heading 1";
+            oPara1.Range.Text = "AX Группа";
             oPara1.Range.Font.Bold = 1;
             oPara1.Format.SpaceAfter = 24;  
             oPara1.Range.InsertParagraphAfter();
@@ -142,7 +246,7 @@ namespace ABC_Analysis_presenter
             wrdRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
             wrdRng.InsertParagraphAfter();
             wrdRng.InsertAfter("THE END.");
-
+            */
             this.Close();
         }
 
@@ -297,8 +401,7 @@ namespace ABC_Analysis_presenter
 
         private void exitBtn_Click(object sender, EventArgs e)
         {
-            mainForm.Visible = true;
-            Close();
+            GenerateDoc();
         }
     }
 }
